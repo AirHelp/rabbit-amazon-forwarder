2153eea960f9cc56c93aa28387582010f570eb0f
maccac <maccac@not.an.email.com>
2018-07-25 21:46:03 +1000

    Adding some happy case tests

--- connector/connector.go
+++ connector/connector.go
@@ -21,7 +21,7 @@ type IOFileReader struct {
 }
 
 func (i *IOFileReader) ReadFile(filename string) ([]byte, error) {
-	return ioutil.ReadFile(os.Getenv(config.CaCertFile))
+	return ioutil.ReadFile(filename)
 }
 
 type CertPoolMaker interface {
@@ -42,8 +42,8 @@ type KeyLoader interface {
 type X509KeyPairLoader struct {
 }
 
-func (x *X509KeyPairLoader) LoadKeyPair(certFile, keyFile string) (tls.Certificate, error) {
-	return tls.LoadX509KeyPair(os.Getenv(config.CertFile), os.Getenv(config.KeyFile))
+func (x *X509KeyPairLoader) LoadKeyPair(certFile string, keyFile string) (tls.Certificate, error) {
+	return tls.LoadX509KeyPair(certFile, keyFile)
 }
 
 type RabbitDialer interface {
@@ -74,51 +74,53 @@ type RabbitConnector interface {
 }
 
 type BasicRabbitConnector struct {
-	basicRabbitDialer *BasicRabbitDialer
+	BasicRabbitDialer RabbitDialer
 }
 
 func (c *BasicRabbitConnector) CreateConnection(connectionURL string) (*amqp.Connection, error) {
 	log.Info("Dialing in")
-	return c.basicRabbitDialer.Dial(connectionURL)
+	return c.BasicRabbitDialer.Dial(connectionURL)
 }
 
 type TlsRabbitConnector struct {
-	tlsConfig     *tls.Config
-	fileReader    FileReader
-	certPoolMaker CertPoolMaker
-	keyLoader     KeyLoader
-	tlsDialer     TlsRabbitDialer
+	TlsConfig     *tls.Config
+	FileReader    FileReader
+	CertPoolMaker CertPoolMaker
+	KeyLoader     KeyLoader
+	TlsDialer     TlsRabbitDialer
 }
 
 func (c *TlsRabbitConnector) CreateConnection(connectionURL string) (*amqp.Connection, error) {
 	log.Info("Dialing in via TLS")
-	c.tlsConfig.RootCAs = c.certPoolMaker.NewCertPool()
-	if ca, err := c.fileReader.ReadFile(os.Getenv(config.CaCertFile)); err == nil {
-		c.tlsConfig.RootCAs.AppendCertsFromPEM(ca)
+	c.TlsConfig.RootCAs = c.CertPoolMaker.NewCertPool()
+	log.Info("1")
+	if ca, err := c.FileReader.ReadFile(os.Getenv(config.CaCertFile)); err == nil {
+		c.TlsConfig.RootCAs.AppendCertsFromPEM(ca)
+		log.Info("2")
 	} else {
 		log.WithField("error", err.Error()).Error("File not found")
 	}
-	if cert, err := c.keyLoader.LoadKeyPair(os.Getenv(config.CertFile), os.Getenv(config.KeyFile)); err == nil {
-		c.tlsConfig.Certificates = append(c.tlsConfig.Certificates, cert)
+	if cert, err := c.KeyLoader.LoadKeyPair(os.Getenv(config.CertFile), os.Getenv(config.KeyFile)); err == nil {
+		c.TlsConfig.Certificates = append(c.TlsConfig.Certificates, cert)
 	} else {
 		log.WithField("error", err.Error()).Error("File not found")
 	}
-	return c.tlsDialer.DialTLS(connectionURL, c.tlsConfig)
+	return c.TlsDialer.DialTLS(connectionURL, c.TlsConfig)
 }
 
 func CreateBasicRabbitConnector() *BasicRabbitConnector {
 	return &BasicRabbitConnector{
-		basicRabbitDialer: &BasicRabbitDialer{},
+		BasicRabbitDialer: &BasicRabbitDialer{},
 	}
 }
 
 func CreateTlsRabbitConnector() *TlsRabbitConnector {
 	return &TlsRabbitConnector{
-		tlsConfig:     new(tls.Config),
-		fileReader:    &IOFileReader{},
-		certPoolMaker: &X509CertPoolMaker{},
-		keyLoader:     &X509KeyPairLoader{},
-		tlsDialer:     &X509TlsDialer{},
+		TlsConfig:     new(tls.Config),
+		FileReader:    &IOFileReader{},
+		CertPoolMaker: &X509CertPoolMaker{},
+		KeyLoader:     &X509KeyPairLoader{},
+		TlsDialer:     &X509TlsDialer{},
 	}
 }
 
--- connector/connector_test.go
+++ connector/connector_test.go
@@ -1,9 +1,12 @@
 package connector_test
 
 import (
+	"crypto/tls"
+
 	"github.com/AirHelp/rabbit-amazon-forwarder/connector"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/streadway/amqp"
 )
 
 var _ = Describe("Connector", func() {
@@ -15,9 +18,7 @@ var _ = Describe("Connector", func() {
 				Expect(actualConnect).Should(BeAssignableToTypeOf(&connector.BasicRabbitConnector{}))
 			})
 		})
-	})
 
-	Describe("Creating a connector", func() {
 		Context("With a tls rabbit configuration", func() {
 			It("should be a TlsRabbitConnector", func() {
 				actualConnect := connector.CreateConnector("amqps")
@@ -25,4 +26,92 @@ var _ = Describe("Connector", func() {
 			})
 		})
 	})
+
+	Describe("Calling connect", func() {
+		Context("With a basic rabbit connector", func() {
+			It("Should create a connection", func() {
+				expectedConnection := createDummyAmqpConnection()
+				dialer := &MockBasicRabbitDialer{
+					ReturnedConnection: expectedConnection,
+					Error:              nil,
+				}
+				rabbitConnector := createBasicConnector(dialer)
+
+				connection, err := rabbitConnector.CreateConnection("any amqp url")
+
+				Expect(connection).Should(Equal(expectedConnection))
+				Expect(err).Should(BeNil())
+			})
+		})
+
+		Context("With a tls rabbit connect", func() {
+			It("Should create a connection", func() {
+				expectedConnection := createDummyAmqpConnection()
+				dialer := &MockTlsRabbitDialer{
+					ReturnedConnection: expectedConnection,
+					Error:              nil,
+				}
+				rabbitConnector := createTlsConnector(dialer)
+				connection, err := rabbitConnector.CreateConnection("any amqp url")
+				Expect(connection).Should(Equal(expectedConnection))
+				Expect(err).Should(BeNil())
+			})
+		})
+	})
 })
+
+func createBasicConnector(mockDialer connector.RabbitDialer) *connector.BasicRabbitConnector {
+	return &connector.BasicRabbitConnector{
+		BasicRabbitDialer: mockDialer,
+	}
+}
+
+func createTlsConnector(mockDialer connector.TlsRabbitDialer) *connector.TlsRabbitConnector {
+	return &connector.TlsRabbitConnector{
+		TlsConfig:     new(tls.Config),
+		FileReader:    &MockFileReader{},
+		CertPoolMaker: &connector.X509CertPoolMaker{},
+		KeyLoader:     &MockKeyPairLoader{},
+		TlsDialer:     mockDialer,
+	}
+}
+
+type MockFileReader struct {
+}
+
+func (i *MockFileReader) ReadFile(filename string) ([]byte, error) {
+	return []byte("Dummy file"), nil
+}
+
+type MockKeyPairLoader struct {
+}
+
+func (x *MockKeyPairLoader) LoadKeyPair(certFile string, keyFile string) (tls.Certificate, error) {
+	return tls.Certificate{}, nil
+}
+
+type MockTlsRabbitDialer struct {
+	Called             bool
+	ReturnedConnection *amqp.Connection
+	Error              error
+}
+
+func (s *MockTlsRabbitDialer) DialTLS(connectionURL string, tlsConfig *tls.Config) (*amqp.Connection, error) {
+	s.Called = true
+	return s.ReturnedConnection, s.Error
+}
+
+type MockBasicRabbitDialer struct {
+	Called             bool
+	ReturnedConnection *amqp.Connection
+	Error              error
+}
+
+func (s *MockBasicRabbitDialer) Dial(connectionURL string) (*amqp.Connection, error) {
+	s.Called = true
+	return s.ReturnedConnection, s.Error
+}
+
+func createDummyAmqpConnection() *amqp.Connection {
+	return &amqp.Connection{}
+}
